38c6baa99b47c4871699bcc94d35fc998f900149
nejim3h <nimitpanwar23@gmail.com>
2025-01-27 18:59:51 +0530

    redux toolkit added

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,43 +1,46 @@
-import { Alert, Label, Spinner, TextInput } from 'flowbite-react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
-  }
-
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if(!formData.email || !formData.password) {
-      return setErrorMessage('All fields are required');
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure('Please fill all the fields'));
     }
-    try{
-      setLoading(true);
-      setErrorMessage(null);
+    try {
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if(data.success === false) {
-        return setErrorMessage(data.message);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if(res.ok) {
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
       }
-    } catch(error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+    } catch (error) {
+      dispatch(signInFailure(error.message));
     }
-  }
+  };
 
   return (
     <div className='min-h-screen mt-20 flex flex-col items-center'>
@@ -47,11 +50,11 @@ export default function SignIn() {
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your email' />
-              <TextInput type='email' placeholder='name@iiitd.ac.in' id='email' onChange={handleChange}/>
+              <TextInput type='email' placeholder='name@iiitd.ac.in' id='email' onChange={handleChange} />
             </div>
             <div>
               <Label value='Your password' />
-              <TextInput type='password' placeholder='***********' id='password' onChange={handleChange}/>
+              <TextInput type='password' placeholder='***********' id='password' onChange={handleChange} />
             </div>
             <button
               type='submit'
@@ -60,8 +63,8 @@ export default function SignIn() {
               {
                 loading ? (
                   <>
-                  <Spinner size='sm' />
-                  <span className='pl-3'>Loading...</span>
+                    <Spinner size='sm' />
+                    <span className='pl-3'>Loading...</span>
                   </>
                 ) : 'Sign In'
               }
